a702dda33ecce23f11a059025385fd71c8b5a8ac
AlexHome <nipu_ro@yahoo.com>
2020-10-17 23:14:52 +0300

    add support for sh shell on alpine containers

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ var child = spawn(`docker`,
     'exec',
     '-it',
     process.argv[2],
-   'bash',
+    process.argv[3] || 'bash',
    '-c',
-   `stty cols ${process.stdout.columns} rows ${process.stdout.rows} && bash`
+   `stty cols ${process.stdout.columns} rows ${process.stdout.rows} && ${process.argv[3] || 'bash'}`
   ], {
     stdio: 'inherit'});
